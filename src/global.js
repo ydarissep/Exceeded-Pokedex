db04fb81a84ea8c67876dc22beacf886a5893f04
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-05-29 06:50:40 +0200

    Add files via upload

--- src/global.js
+++ src/global.js
@@ -1,6 +1,7 @@
 window.repo = "benicioneto/Exceeded-Pokedex/main"
 window.repoTypeChartUrl = "https://raw.githubusercontent.com/ydarissep/Exceeded-Pokedex/main/src/typeChart.json"
 window.checkUpdate = "21 EE"
+window.showShinyToggle = true
 //window.innatesDefined = true
 
 
